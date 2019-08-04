dec01d17452946a2f84349524398cf96f17ae2ed
Blake Mitchell <bam365@mst.edu>
2019-08-04 10:05:40 -0500

    initial commit

---
+++ src/components/PomodoroTimer.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const PomodoroTimer = () => (
+    <h1>Pomodoro Timer</h1>
+)
+
+export default PomodoroTimer;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import PomodoroTimer from './components/PomodoroTimer';
+
+ReactDOM.render(<PomodoroTimer />, document.getElementById('app'));
